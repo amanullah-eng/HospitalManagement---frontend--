8fd28cde9afc93daccfc41cdb3779dff1549f98f
aman <aman435islam@gmail.com>
2025-07-13 01:37:14 +0600

    update login
    modify

--- src/app/Blank/login/login.component.ts
+++ src/app/Blank/login/login.component.ts
@@ -17,88 +17,69 @@ export class LoginComponent implements OnInit{
 
   constructor(private router: Router, private userService : UserService) { }
   ngOnInit(): void {
-    
-  }
 
+  }
 
 roles: string[] = ['AdminPanel', 'Doctor', 'Patient', 'Staff', 'LabPanel'];
 
-
-login(){
-  this.userService.login(this.user).subscribe(
-  (res: any) => {
-    localStorage.setItem('loggedInUser', res.username);
-    localStorage.setItem('userRole', res.role);
-    localStorage.setItem('userId', res.id); 
-    this.router.navigate(['/profile']);
-    console.log(this.user); 
-
-  },
-  (error) => {
-    alert('Login failed!');
-  }
-);
-
-}
-
-//   login() {
-//   this.userService.login(this.user).subscribe({
-//     next: (res: any) => {
-//       const role = res.role;
-//       const username = res.username;
+  login() {
+  this.userService.login(this.user).subscribe({
+    next: (res: any) => {
+      const role = res.role;
+      const username = res.username;
       
-//          if(this.user.password == res.password){           
-//             Swal.fire({
-//               title: "logged in",
-//               icon: "success",
-//               draggable: true
-//             });
-//          }
+         if(this.user.password == res.password){           
+            Swal.fire({
+              title: "logged in",
+              icon: "success",
+              draggable: true
+            });
+         }
 
-//       // Store in localStorage if needed
-//       localStorage.setItem('loggedInUser', res.username);
-//     localStorage.setItem('userRole', res.role);
-//     localStorage.setItem('userId', res.id); // For image upload
-//     this.router.navigate(['/profile']);
+      // Store in localStorage if needed
+      localStorage.setItem('loggedInUser', res.username);
+    localStorage.setItem('userRole', res.role);
+    localStorage.setItem('userId', res.id); // For image upload
+    this.router.navigate(['/profile']);
 
-//       // Navigate by role
-//       switch (role) {
-//         case 'AdminPanel':
-//           this.router.navigate(['/admin']);
-//           break;
+      // Navigate by role
+      switch (role) {
+        case 'AdminPanel':
+          this.router.navigate(['/admin']);
+          break;
 
-//         case 'Doctor':
-//           this.router.navigate(['/doctorDas']);
-//           break;
+        case 'Doctor':
+          this.router.navigate(['/doctorDas']);
+          break;
 
-//         case 'Patient':
-//           this.router.navigate(['/patientDas']);
-//           break;
+        case 'Patient':
+          this.router.navigate(['/patientDas']);
+          break;
 
-//         case 'LabPanel':
-//           this.router.navigate(['/labDas']);
-//           break;
+        case 'LabPanel':
+          this.router.navigate(['/labDas']);
+          break;
 
-//         case 'Staff':
-//           this.router.navigate(['/staff-dashboard']);
-//           break;
+        case 'Staff':
+          this.router.navigate(['/staff-dashboard']);
+          break;
 
-//         default:
-//           this.router.navigate(['/unauthorized']);
-//           break;
-//       }
-//     },
-//     error: (err) => {
-//         Swal.fire({
-//           title: "login faild",
-//           icon: "success",
-//           draggable: true
-//         });
+        default:
+          this.router.navigate(['/unauthorized']);
+          break;
+      }
+    },
+    error: (err) => {
+        Swal.fire({
+          title: "login faild",
+          icon: "success",
+          draggable: true
+        });
 
-//       console.error(err);
-//     }
-//   });
-// }
+      console.error(err);
+    }
+  });
+}
 
 
 }
